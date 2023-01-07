379d7d3a3ca2097ea14c51ee0f8081b333587128
dBerriff <berriff@icloud.com>
2023-01-07 22:28:36 +0000

    Rebuild GitHub repository

--- switches_as.py
+++ switches_as.py
@@ -3,54 +3,68 @@ from machine import Pin
 import uasyncio as asyncio
 
 
-class HwSwIn(Pin):
+class HwSwitch(Pin):
     """ on/off switch (latching) input
         - extends machine.Pin class """
     # class constants
-    # PULL_UP logic: pin connected to ground for ON
+    # pull-up logic
     _OFF = 1
     _ON = 0
  
-    def __init__(self, pin):
+    def __init__(self, pin: int):
+        # initialise Pin parent object
+        super().__init__(pin, Pin.IN, Pin.PULL_UP)
         self.pin = pin
-        pin_ = int(pin)
-        super().__init__(pin_, Pin.IN, Pin.PULL_UP)
         self._state = None
 
     @property
     def state(self):
         """ return physical switch state  """
-        self._state = 1 if self.value() == HwSwIn._ON else 0
+        self._state = 1 if self.value() == HwSwitch._ON else 0
         return self._state
-
-
-def pin_switch(pins):
-    """ return dictionary of pin: switch-objects """
-    pin_sw = {pin: HwSwIn(pin) for pin in pins}
-    return pin_sw
-
-
-def pin_state(switches):
-    """ return dictionary of pin: switch-states """
-    pin_state_ = {pin: switches[pin].state for pin in switches}
-    return pin_state_
     
+
+class SwitchSet:
+    """ poll and return switch settings """
+
+    def __init__(self, switch_pins_: tuple, poll_interval: int = 200):
+        self.switches = {pin: HwSwitch(pin) for pin in switch_pins_}
+        self.poll_interval = poll_interval
+        self.ev_input = asyncio.Event()
+        self._pin_states = None
+        self._previous_states = None
+    
+    @property
+    def pin_states(self):
+        """ return dictionary of pin states """
+        return self._pin_states
+
+    async def poll_switches(self):
+        """ poll switch states and respond to change """
+        while True:
+            self._pin_states = {
+                pin: self.switches[pin].state for pin in self.switches}
+            if self._pin_states != self._previous_states:
+                self.ev_input.set()
+            await asyncio.sleep_ms(self.poll_interval)
+            self._previous_states = self._pin_states
+
 
 def main():
     """ test polling of switch inputs """
+    # === user parameters
     
-    from util_as import heartbeat
-    asyncio.create_task(heartbeat())
-    # test data
     switch_pins = (16, 17, 18)
-    switches = pin_switch(switch_pins)
     
-    poll_interval = 1000  # ms
+    # ===
+    
+    switches = SwitchSet(switch_pins)
+    asyncio.create_task(switches.poll_switches())
     while True:
-        states = pin_state(switches)
-        print(states)
-        await asyncio.sleep_ms(poll_interval)
-        
+        print(f'pin: switch state {switches.pin_states}')
+        await switches.ev_input.wait()
+        switches.ev_input.clear()
+
 
 if __name__ == '__main__':
     try:
