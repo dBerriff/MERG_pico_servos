033ec7dfef55f15ec15e6cdd0a3be25b57301bde
dBerriff <berriff@icloud.com>
2023-01-09 15:04:42 +0000

    Add to repository

--- switches_as.py
+++ switches_as.py
@@ -15,7 +15,7 @@ class HwSwitch(Pin):
     def __init__(self, pin: int):
         # initialise Pin parent object
         super().__init__(pin, Pin.IN, Pin.PULL_UP)
-        self.pin = pin
+        self.pin = pin  # required as switch ID
         self._state = None
 
     @property
@@ -32,52 +32,57 @@ class SwitchSet:
         self.switches = {pin: HwSwitch(pin) for pin in switch_pins_}
         self.poll_interval = poll_interval
         self.ev_input = asyncio.Event()  # set when input data received
-        self.ev_main_ready = asyncio.Event()  # set when data consumer is ready
-        self._pin_states = None
+        self.ev_consumer_ready = asyncio.Event()  # set when data consumer is ready
+        self._switch_states = None
         self._previous_states = None
     
-    @property
-    def pin_states(self):
-        """ return dictionary of pin states """
-        return self._pin_states
+    @property  # not required but used for consistency
+    def switch_states(self):
+        """ return dictionary of switch states """
+        return self._switch_states
 
     async def poll_switches(self):
         """ poll switch states and respond to change """
         while True:
-            self._pin_states = {
+            self._switch_states = {
                 pin: self.switches[pin].state for pin in self.switches}
-            if self._pin_states != self._previous_states:
+            if self._switch_states != self._previous_states:
                 self.ev_input.set()
             await asyncio.sleep_ms(self.poll_interval)
-            await self.ev_main_ready.wait()
-            self._previous_states = self._pin_states
+            await self.ev_consumer_ready.wait()
+            self._previous_states = self._switch_states
+    
+    def print_switch_states(self):
+        """ print switch states """
+        print(f'pin: switch state {self._switch_states}')
 
 
 # === test / demo code
 
-async def print_switch_status(switches_):
+async def consume_switch_data(switches_):
     """ print switch status when changed """
-    print(f'pin: switch state {switches_.pin_states}')
+    print(f'pin: switch state {switches_.switch_states}')
     for _ in range(10):
-        switches_.ev_main_ready.set()    # main() is ready for data
-        await switches_.ev_input.wait()  # wait for data input
-        switches_.ev_input.clear()       # data input acknowledged
-        switches_.ev_main_ready.clear()  # flag main() as busy
-        print(f'pin: switch state {switches_.pin_states}')
-        sleep_ms(2_000)                 # simulate servo setting 
+        switches_.ev_consumer_ready.set()  # flag consumer ready for data
+        await switches_.ev_input.wait()  # await ev_input is set
+        switches_.ev_input.clear()  # clear event ready for next set
+        switches_.ev_consumer_ready.clear()  # flag consumer as busy
+        switches_.print_switch_states()
+        sleep_ms(2_000)  # simulate servo setting
 
 
 def main():
     """ test polling of switch inputs """
+    
     # === user parameters
     
     switch_pins = (16, 17, 18)
     
-    # ===
+    # === end user parameters
     
     switches = SwitchSet(switch_pins)
     asyncio.create_task(switches.poll_switches())
-    await print_switch_status(switches)
+    await consume_switch_data(switches)
 
 
 if __name__ == '__main__':
